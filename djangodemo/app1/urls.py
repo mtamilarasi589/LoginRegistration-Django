7487f6a3a2a68257a9ef8be58eeb7bf4d9e79291
mtamilarasi <mtamilarasi589@gmail.com>
2024-09-27 11:17:36 +0530

    first commit

--- djangodemo/app1/urls.py
+++ djangodemo/app1/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
      path("register",views.register,name="register"),
      path('login',views.login_page,name="login"),
      path('logout',views.logout_page,name="logout"),
-    
+
 ]
